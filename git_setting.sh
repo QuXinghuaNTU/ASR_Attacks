d7983614c715cd8283142233cf0ffa29e311263b
Qu Xinghua <36146785+QuXinghuaNTU@users.noreply.github.com>
2021-07-17 09:28:20 +0800

    Update git_setting.sh

--- git_setting.sh
+++ git_setting.sh
@@ -1,3 +1,10 @@
 git config --global user.name "QuXinghuaNTU"
 git config --global user.email xinghua001@e.ntu.edu.sg
 git config --list
+
+git remote remove origin
+git remote add origin git@github.com:QuXinghuaNTU/ASR_Attacks.git
+git commit -m 'the commit for ref'
+git push origin HEAD:main # do not use git push origin master
+user: QuXinghuaNTU
+token: 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
